4093b99d1ba2210288b03052387ea232c9a4e758
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-10-24 22:11:55 +0100

    create and finalised delete and add movie programs

--- AddMovie.py
+++ AddMovie.py
@@ -1,42 +1,45 @@
 from connect import *
 
 def insertSong(): 
-    movieTitle = input('Movie Title: ')
+    print('\n\n\n')
+    movieTitle = input('Movie Title: ').title()
     releaseYear = int(input('Year Of Release: '))
     duration = int(input('Duration (minutes): '))
-    
-    # MULTI-CHOICE INPUTS -------
+    rating = getContentRating()
+    genre = getGenre().title()
 
-    # rating ----
-    ratingInt = int(input('Content Rating:\n1. G - General Audiences\n2. PG - Parental Guidence Advised\n3. R - Restricted\n--:'))
-    if ratingInt == 1:
-        rating = 'G'
-    elif ratingInt == 2:
-        rating = 'PG'
-    elif ratingInt == 3:
-        rating = 'R'
+    record = ' | '.join([movieTitle, str(releaseYear), (str(duration) + ' mins'), rating, genre])
+    if confirmInsert( record ):
+        dbCursor.execute('INSERT INTO tblFilms VALUES ( NULL, ?, ?, ?, ?, ? )', ( movieTitle, releaseYear, rating, duration, genre ))
+        dbCon.commit()
+        print('Movie has been inserted...')
+    else:
+        print('\n\nInsert cancelled...')
 
-    # genre ----
-    genreInt = int(input('Genre:\n1. Action\n2. Animation\n3. Crime\n4. Comedy\n5. Fantasy\n6. Fighting\n7. Sci-Fi\n8. Adventure\n--:'))
-    if genreInt == 1:
-        genre = 'Action'
-    elif genreInt == 2:
-        genre = 'Animation'
-    elif genreInt == 3:
-        genre = 'Crime'
-    elif genreInt == 4:
-        genre = 'Comedy'
-    elif genreInt == 5:
-        genre = 'Fantasy'
-    elif genreInt == 6:
-        genre = 'Fighting'
-    elif genreInt == 7:
-        genre = 'Sci-Fi'
-    elif genreInt == 8:
-        genre = 'Adventure'
+def getContentRating():
+    print('\n\n\n')
+    ratingInt = int(input('Content Rating:\n-----------\n1. G - General Audiences\n2. PG - Parental Guidence Advised\n3. R - Restricted\n--: '))
+    ratingList = ['G','PG','R']
+    rating = ratingList[ratingInt-1]
+    return rating
 
-    dbCursor.execute('INSERT INTO tblFilms VALUES ( NULL, ?, ?, ?, ?, ? )', ( movieTitle, releaseYear, duration, rating, genre ))
-    dbCon.commit()
+def getGenre():
+    print('\n\n\n')
+    genreInt = int(input('Genre:\n1. Action\n2. Animation\n3. Crime\n4. Comedy\n5. Fantasy\n6. Fighting\n7. Sci-Fi\n8. Adventure\n--: '))
+    genreList = ['Action','Animation','Crime','Comedy','Fantasy','Fighting','Sci-Fi','Adventure']
+    genre = genreList[genreInt-1]
+    return genre
+
+def confirmInsert( entry ):
+    optionList = ['Y','N']
+    confirmation = input(f'Please confirm you would like to confirm the following insert (Y/N):\n-----------\n{ entry }\n-----------\n---: ')
+    while confirmation.upper() not in optionList:
+        print('\n\n--- Invalid Selection\n\n')
+        confirmation = input(f'Please confirm you would like to confirm the following insert (Y/N):\n-----------\n{ entry }\n-----------\n---: ')
+    if confirmation.upper() == 'Y':
+        return  True
+    if confirmation.upper() == 'N':
+        return False
 
 if __name__ == '__main__':
     insertSong()
--- DeleteMovie.py
+++ DeleteMovie.py
@@ -1,10 +1,11 @@
 from connect import *
 
 def inputFromAvailible( column ):
-    dbCursor.execute('SELECT DISTINCT ? FROM tblFilms', column )
+    query = f'SELECT DISTINCT { column } FROM tblFilms'
+    dbCursor.execute( query )
     results = dbCursor.fetchall()
     counter = 1
-    inputString = 'Please select from following options:'
+    inputString = '\n\nPlease select from following options:\n-----------'
     checkList = []
 
     for item in results:
@@ -14,39 +15,89 @@ def inputFromAvailible( column ):
         checkList.append(itemString)
         counter += 1
 
-    value = input(f'{ inputString }\n\n---: ')
+    selection = input(f'{ inputString }\n\n---: ')
 
-    while value not in checkList:
+    while int(selection) not in range(1,len(checkList)):
         print('\n\n--- Invalid Selection\n\n')
-        value = input(f'{ inputString }\n\n---: ')
+        selection = input(f'{ inputString }\n\n---: ')
     
+    value = checkList[int(selection)-1]
+
     return value
 
+def getDeletionValue( column ):
+    if column == 'title' or column == 'filmID':
+        return input(f'Please enter the { column }: ')
+    else:
+        return inputFromAvailible( column )
 
-# def getDeletionValue( column ):
-#     if column == 'title' or column == 'filmID':
-#         return input(f'Please enter the { column }: ')
-#     else:
-#         return inputFromAvailible( column )
-        
+def getDeletionColumn():
+    inputText = 'Choose to delete by:\n-----------\n1. Movie ID\n2. Movie Title\n\nOr\n\nDelete all films of specied...\n-----------\n3. Year Of Release\n4. Rating\n5. Genre\n----: '
+    
+    deleteByInt = int(input(inputText))
 
-# def getDeletionColumn():
-#     deleteByInt = input('Choose to delete by:\n1. Movie ID\n2. Movie Title\n\nOr\n\nDelete all films of specied...\n3. Year Of Release\n4. Rating\n5. Genre')
-#     if deleteByInt == 1:
-#         deleteBy = 'filmID'
-#     elif deleteByInt == 2:
-#         deleteBy = 'title'
-#     elif deleteByInt == 3:
-#         deleteBy = 'yearReleased'
-#     elif deleteByInt == 4:
-#         deleteBy = 'rating'
-#     elif deleteByInt == 5:
-#         deleteBy = 'genre'
-#     return deleteBy
+    while deleteByInt not in range( 1, 6 ):
+        print('\n\n!!Invalid Selection!!\n')
+        deleteByInt = int(input(inputText))
 
-# def DeleteMovie():
-#     deletionColumn = getDeletionColumn()
-#     deletionValue = getDeletionValue( deletionColumn )
-#     deletionQuery = f'DELETE FROM tblFilms WHERE { deletionColumn } = ?'
-#     dbCursor.execute( deletionQuery, deletionValue )
+    if deleteByInt == 1:
+        deleteBy = 'filmID'
+    elif deleteByInt == 2:
+        deleteBy = 'title'
+    elif deleteByInt == 3:
+        deleteBy = 'yearReleased'
+    elif deleteByInt == 4:
+        deleteBy = 'rating'
+    elif deleteByInt == 5:
+        deleteBy = 'genre'
 
+    return deleteBy
+
+def constructionComfirmationString( itemList ):
+    toBeDeletedStr = f'\n\nThe following will be deleted:\n-----------'
+    if len(itemList) > 1:
+        for item in itemList:
+            itemStr = f'\n{item[0]}'
+            toBeDeletedStr += itemStr
+    elif len(itemList) == 1:
+        toBeDeletedStr += f'  { itemList[0][0] }'
+    else:
+        print('Movie does not exist')
+    return toBeDeletedStr
+
+def getConfirmation( comfirmationStr ):
+    optionList = ['Y','N']
+    confirmation = input( comfirmationStr + '\n-----------\nPlease confirm (Y/N)\n---: ')
+    while confirmation.upper() not in optionList:
+        print('\n\n--- Invalid Selection\n\n')
+        confirmation = input( comfirmationStr + '\n-----------\nPlease confirm (Y/N)\n---: ')
+    if confirmation.upper() == 'Y':
+        return  True
+    if confirmation.upper() == 'N':
+        return False
+    
+def DeleteMovie():
+    deletionColumn = getDeletionColumn()
+    deletionValue = getDeletionValue( deletionColumn )
+    if deletionColumn == 'filmID':
+        condition = str(deletionValue)
+    else:
+        condition = (f'%{ deletionValue }%')
+
+    confirmationQuery = f"SELECT title FROM tblFilms WHERE LIKE( ?, { deletionColumn })"
+
+    dbCursor.execute( confirmationQuery, [condition] )
+    toBeDeleted = dbCursor.fetchall()
+
+    if len(toBeDeleted) >= 1:
+        comfirmationString = constructionComfirmationString( toBeDeleted )
+        if getConfirmation( comfirmationString ):
+            deletionQuery = f'DELETE FROM tblFilms WHERE LIKE( ?, { deletionColumn })'
+            dbCursor.execute( deletionQuery, [condition] )
+            dbCon.commit()
+            print(f'\n\nItem(s) have been deleted\n\n')
+        elif not getConfirmation( comfirmationString ):
+            print('\n\nItem(s) will not be deleted\n\n')
+            
+if __name__ == '__main__':
+    DeleteMovie()
